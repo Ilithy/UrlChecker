241f1125fb4de11ee3c1ad1657067eccdcc59494
Ilithy <36798218+Ilithy@users.noreply.github.com>
2022-08-25 16:21:32 +0200

    Revert "Test clipboard changes (#8)"
    This reverts commit be3ea0c0ec6d918992f0b8a34404e6febb90874e.

--- app/src/main/java/com/trianguloy/urlchecker/modules/list/OpenModule.java
+++ app/src/main/java/com/trianguloy/urlchecker/modules/list/OpenModule.java
@@ -275,10 +275,6 @@ class OpenDialog extends AModuleDialog implements View.OnClickListener, PopupMen
         ClipData clip = ClipData.newPlainText("", getUrl());
         if (clipboard != null) {
             clipboard.setPrimaryClip(clip);
-            // Only show a toast for Android 12L and lower.
-        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.S_V2){
-              description.extras = PersistableBundle().apply {
-                  putBoolean("android.content.extra.IS_SENSITIVE", sensitive)
             Toast.makeText(getActivity(), R.string.mOpen_clipboard, Toast.LENGTH_LONG).show();
         }
     }
